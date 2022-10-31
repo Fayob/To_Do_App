95e00acad7fbe55527d05500de5604a5b087ab65
= <adedejifavour6@gmail.com>
2022-10-31 15:26:13 +0100

    Add input field component

---
+++ src/component/InputTodo.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const InputTodo = ({ addTodoItem }) => {
+  const [state, setState] = useState({
+    title: ''
+  })
+
+  const onChange = (e) => {
+    setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!state.title.trim()) {
+      alert('Please write item')
+      return
+    }
+    addTodoItem(state.title);
+    setState({
+      title: ''
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className='form-container'>
+      <input
+        type="text"
+        name="title"
+        className='input-text'
+        value={state.title}
+        onChange={onChange}
+        placeholder='Add Todo...' />
+      <button className='input-submit'>Submit</button>
+    </form>
+  )
+}
+
+export default InputTodo
