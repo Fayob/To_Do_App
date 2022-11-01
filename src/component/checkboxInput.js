7b155ac592c46acfb2e7d84840796df39dcb3b19
= <adedejifavour6@gmail.com>
2022-11-01 09:35:06 +0100

    Modified checkbox functionality

--- src/component/checkboxInput.js
+++ src/component/checkboxInput.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Checkbox = ({id, completed, checkboxHandler}) => {
-  return (
-    <div className='checkbox'>
-      <input type="checkbox" checked={completed} onChange={() => checkboxHandler(id)} />
-    </div>
-  )
-}
+const Checkbox = ({ id, completed, checkboxHandler }) => (
+  <div className="checkbox">
+    <input type="checkbox" checked={completed} onChange={() => checkboxHandler(id)} />
+  </div>
+);
 
-export default Checkbox
+Checkbox.propTypes = {
+  id: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired,
+  checkboxHandler: PropTypes.func.isRequired,
+};
+
+export default Checkbox;
